e1fcf87bfe8b3f2683e2d87402b252632c8dedf2
lorwaysan <761472987@qq.com>
2019-08-03 02:09:36 +0800

    update 20190803

--- alien_invasion/main.py
+++ alien_invasion/main.py
@@ -10,7 +10,7 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))  ##初始化要显示的窗口或屏幕
     pygame.display.set_caption("Alien Invasion")        #设置当前窗口标题
 
-    ship = Ship(screen)     #创建一艘飞船
+    ship = Ship(ai_settings,screen)     #创建一艘飞船
 
     while True:
         gf.check_events(ship)
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,10 +1,11 @@
 import pygame
 
 class Ship():
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         '''初始化飞船并设置其初始位置'''
         
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('ship.bmp')
@@ -24,9 +25,9 @@ class Ship():
 
     def update(self):
         '''根据移动标志调整飞船的位置'''
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.rect.centerx += self.ai_settings.ship_speed_factor
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > 0:
             self.rect.centerx -= self.ai_settings.ship_speed_factor
         
         #根据self.center更新rect对象
